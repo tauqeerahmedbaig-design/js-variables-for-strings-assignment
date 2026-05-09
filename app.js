d79d798a75b7ec68fdd04e9bda8c0aee998214c8
Tauqeer Ahmed baig <tauqeerahmedbaig@gmail.com>
2026-05-10 00:43:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+// Exercise 1
+var firstName = "Shahzad";
+alert(firstName);
+
+// Exercise 2
+var lastName = "Ahmed";
+alert(firstName + " " + lastName);
+
+// Exercise 3
+var message = "Welcome to JavaScript!";
+alert(message);
+
+// Exercise 4
+var city = "Karachi";
+alert("Welcome to " + city);
+
+// Exercise 5
+var food = "Biryani";
+alert("I like to eat " + food);
+
+// Exercise 6
+var part1 = "Java";
+var part2 = "Script";
+alert(part1 + part2);
+
+// Exercise 7
+var word1 = "Hello";
+var word2 = "World";
+alert(word1 + " " + word2);
+
+// Exercise 8
+var sentence = "Learning JavaScript is fun!";
+alert(sentence);
+
+// Exercise 9
+var country = "Pakistan";
+alert(country);
+
+// Exercise 10
+var game = "Cricket";
+alert("My favorite game is " + game);
